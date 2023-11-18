982d32f31c468a605d1c68426b972a8835fef349
Mike-P63 <rumaish@rambler.ru>
2023-11-18 21:53:22 +0400

    added changes conftest

--- test_1.py
+++ test_1.py
@@ -21,7 +21,10 @@ def get_sites(lat, long, radius, limit=100):
 
 # print(get_sites("37.7891838", "-122.4033522", 100))
 
-def test_step1():
-    assert "One Montgomery Tower" in get_sites("37.7891838", "-122.4033522", 100), "Not found"
+# def test_step1():
+#     assert "One Montgomery Tower" in get_sites("37.7891838", "-122.4033522", 100), "Not found"
 
-test_step1()
+# test_step1()
+
+def test_step1(coord1, text1):
+    assert text1 in get_sites(coord1[0], coord1[1], 100), "not found"
