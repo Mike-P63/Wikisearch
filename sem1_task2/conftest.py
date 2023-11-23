26240bcfd058a5c3afbb63140787aaee55f8d60a
Mike-P63 <rumaish@rambler.ru>
2023-11-23 21:06:42 +0400

    HW_1 task added

--- sem1_task2/conftest.py
+++ sem1_task2/conftest.py
@@ -1,14 +1,41 @@
 import pytest
 import requests
+import yaml
 
-url = "https://test-stand.gb.ru/gateway/login"
-login = "kitty89"
-password = "61d96a3985"
+with open('config.yaml', encoding='utf-8') as f:
+    data = yaml.safe_load(f)
+
+Session = requests.Session()
+
+@pytest.fixture()
+def user_login():
+    result = Session.post(url=data['url'], data={'username': data['login'], 'password': data['pswd']})
+    response_json = result.json()
+    token = response_json.get('token')
+    return token
 
 
-result = requests.post(url=url, data={"username": login, "password": password})
-token = result.json()["token"]
+@pytest.fixture()
+def get_description():
+    return 'New_blog_for_HW_1'
 
-res_get = requests.get(url=" https://test-stand.gb.ru/api/posts", headers={"X-Auth-Token": token}, params={"owner":"notMe"})
-print(res_get)
-print(res_get.json())
+@pytest.fixture()
+def post_title():
+    return 'TestTitle' 
+
+
+
+
+
+
+# url = "https://test-stand.gb.ru/gateway/login"
+# login = "AlenaK"
+# password = "d02d48181c"
+
+
+# result = requests.post(url=url, data={"username": login, "password": password})
+# token = result.json()["token"]
+
+# res_get = requests.get(url=" https://test-stand.gb.ru/api/posts", headers={"X-Auth-Token": token}, params={"owner":"notMe"})
+# print(res_get)
+# print(res_get.json())
