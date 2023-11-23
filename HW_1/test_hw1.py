6c3cf87525cbc7ebadd9047db3ca77817664c3a1
Mike-P63 <rumaish@rambler.ru>
2023-11-23 22:21:11 +0400

    HW 1 task added

---
+++ HW_1/test_hw1.py
@@ -0,0 +1,24 @@
+import pytest
+import requests
+import yaml
+
+with open('config.yaml', encoding='utf-8') as f:
+    data = yaml.safe_load(f)
+
+Session = requests.Session()
+
+def test_post_create(user_login):
+    res = Session.post(url=data['url3_post'], headers={'X-Auth-Token': user_login},
+    data={'title': data['title'], 'description': data['description'], 'content': data['content']})
+    assert str(res) == '<Response [200]>', 'post_create FAIL'
+
+
+def test_check_post_create(user_login, get_description):
+
+    result = Session.get(url=data['url2'], headers={'X-Auth-Token': user_login}).json()['data']
+    list_description = [i['description'] for i in result]
+    assert get_description in list_description, 'check_post_create FAIL'
+
+
+if __name__ == "__main__":
+    pytest.main(["-vv"])
